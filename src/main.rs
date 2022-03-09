2b007a24e784c2b0d8197518e9f99e0db92c5a1f
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-03-09 15:59:40 +0300

    Add: first compilable example.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
-use cortex_m_rt::entry;
+//extern crate cortex_m;
+//extern crate panic_itm;
 
-#[entry]
-fn main() -> ! {
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    entrypoint()
+}
+
+fn entrypoint() -> ! {
     let _y;
     let x = 42;
     _y = x;
